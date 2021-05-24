871d9bd8c9e72aad1800c5160c53c4e02cefcbd1
Excavelty <gambitek2@gmail.com>
2021-05-24 22:55:51 +0200

    =Downloaded and prepared uci's data files and loaded them in Matlab

---
+++ main.m
@@ -0,0 +1,10 @@
+clear;clc;
+
+iris_names = ["Setosa", "Versicolor", "Viriginica"];
+
+% Nazwy w zbiorze iris zostaly zastepione przez liczbowe etykiety (1, 2, 3)
+% aby latwiej bylo wczytac zbior danych
+iris_data = load('iris.data');
+wine_data = load('wine.data');
+seeders_data = load('seeds_dataset.txt');
+
